80547ad84f3cbb652f0ddb9b7a2bf7b09800f9e2
Mike Vollmer <mike@recurial.com>
2015-07-24 16:49:18 +0100

    writing big step semantics

--- Lang.thy
+++ Lang.thy
@@ -11,13 +11,6 @@ An array is a list.
 *}
 type_synonym 'a array = "'a list"
 
-text {*
-Currently the only scalar value is @{text int}. They can be put in 
-arrays and tuples.
-*}
-datatype scalar_const = IntC int
-datatype const = ScalarC scalar_const | TupleC const const | ArrayC "const array"
-
 subsection "Abstract Syntax Tree"
 
 text {* Variable bindings are handled via strings for now. *}
@@ -29,7 +22,7 @@ basically an extension of the lambda calculus with collective array
 operations.
  *}
 datatype exp = 
-    Const const         (* introduce a const value *)
+    CInt int            (* introduce a constant int value *)
   | Add exp exp         (* add two scalar values *)
   | Sub exp exp         (* subtract two scalar values *)
   | Mul exp exp         (* multiply two scalar values *)
@@ -37,7 +30,8 @@ datatype exp =
   | PrjR exp            (* project the right part of a tuple *)
   | Lam var exp         (* lambda abstraction *)
   | App exp exp         (* apply a lambda function to an expression *)
-  | Tup exp exp         (* create a tuple *)
+  | Tup exp exp         (* a tuple *)
+  | Array "exp array"   (* an array of expressions *)
   | Var var             (* look up a value in the environment *)
   | Map exp exp         (* apply a lambda function to every element in an array *)
   | Zip exp exp         (* zip two arrays to form an array of tuples *)
@@ -56,7 +50,7 @@ datatype ty =
 subsection "Semantics"
 
 fun subst :: "exp \<Rightarrow> var \<Rightarrow> exp \<Rightarrow> exp" where
-    "subst (Const c) v e' = Const c"
+    "subst (CInt c) v e' = CInt c"
   | "subst (Add e1 e2) v e' = Add (subst e1 v e') (subst e2 v e')"
   | "subst (Sub e1 e2) v e' = Sub (subst e1 v e') (subst e2 v e')"
   | "subst (Mul e1 e2) v e' = Mul (subst e1 v e') (subst e2 v e')"
@@ -66,6 +60,7 @@ fun subst :: "exp \<Rightarrow> var \<Rightarrow> exp \<Rightarrow> exp" where
       (if v' = v then (Lam v' e) else (Lam v' (subst e v e')))"
   | "subst (App e1 e2) v e' = App (subst e1 v e') (subst e2 v e')"
   | "subst (Tup e1 e2) v e' = Tup (subst e1 v e') (subst e2 v e')"
+  | "subst (Array es) v e' = Array (map (\<lambda>i. subst i v e') es)"
   | "subst (Var v') v e' = 
       (if v' = v then e' else (Var v'))"
   | "subst (Map e1 e2) v e' = Map (subst e1 v e') (subst e2 v e')"
@@ -84,21 +79,36 @@ datatype result = Res exp | Error
 inductive
   eval :: "exp \<Rightarrow> result \<Rightarrow> bool" (infix "\<leadsto>" 70)
 where
-    Const: "(Const c) \<leadsto> Res (Const c)"
-  | Add: "\<lbrakk> (e1) \<leadsto> Res (Const (ScalarC (IntC c1))); (e2) \<leadsto> Res (Const (ScalarC (IntC c2))) \<rbrakk>
-           \<Longrightarrow> (Add e1 e2) \<leadsto> Res (Const (ScalarC (IntC (c1 + c2))))"
-  | Sub: "\<lbrakk> (e1) \<leadsto> Res (Const (ScalarC (IntC c1))); (e2) \<leadsto> Res (Const (ScalarC (IntC c2))) \<rbrakk>
-           \<Longrightarrow> (Add e1 e2) \<leadsto> Res (Const (ScalarC (IntC (c1 - c2))))"
-  | Mul: "\<lbrakk> (e1) \<leadsto> Res (Const (ScalarC (IntC c1))); (e2) \<leadsto> Res (Const (ScalarC (IntC c2))) \<rbrakk>
-           \<Longrightarrow> (Add e1 e2) \<leadsto> Res (Const (ScalarC (IntC (c1 * c2))))"
-  | PrjL: "\<lbrakk> (e) \<leadsto> Res (Const (TupleC c1 c2)) \<rbrakk>
-           \<Longrightarrow> (PrjL e) \<leadsto> Res (Const c1)"
-  | PrjR: "\<lbrakk> (e) \<leadsto> Res (Const (TupleC c1 c2)) \<rbrakk>
-           \<Longrightarrow> (PrjL e) \<leadsto> Res (Const c2)"
+    Int: "(CInt c) \<leadsto> Res (CInt c)"
+  | Add: "\<lbrakk> (e1) \<leadsto> Res (CInt c1); (e2) \<leadsto> Res (CInt c2) \<rbrakk>
+          \<Longrightarrow> (Add e1 e2) \<leadsto> Res (CInt (c1 + c2))"
+  | Sub: "\<lbrakk> (e1) \<leadsto> Res (CInt c1); (e2) \<leadsto> Res (CInt c2) \<rbrakk>
+          \<Longrightarrow> (Add e1 e2) \<leadsto> Res (CInt (c1 - c2))"
+  | Mul: "\<lbrakk> (e1) \<leadsto> Res (CInt c1); (e2) \<leadsto> Res (CInt c2) \<rbrakk>
+          \<Longrightarrow> (Add e1 e2) \<leadsto> Res (CInt (c1 * c2))"
+  | PrjL: "\<lbrakk> (e) \<leadsto> Res (Tup c1 c2) \<rbrakk>
+          \<Longrightarrow> (PrjL e) \<leadsto> Res c1"
+  | PrjR: "\<lbrakk> (e) \<leadsto> Res (Tup c1 c2) \<rbrakk>
+          \<Longrightarrow> (PrjL e) \<leadsto> Res c2"
   | Lam: "(Lam v e) \<leadsto> Res (Lam v e)"
   | App: "\<lbrakk> (e1) \<leadsto> Res (Lam v e); (e2) \<leadsto> Res e2'; (subst e v e2') \<leadsto> e' \<rbrakk>
           \<Longrightarrow> (App e1 e2) \<leadsto> e'"
+  | Array1: "(Array []) \<leadsto> Res (Array [])"
+  | Array2: "\<lbrakk> (Array es) \<leadsto> Res (Array as); (e) \<leadsto> Res e' \<rbrakk>
+          \<Longrightarrow> (Array (e # es)) \<leadsto> Res (Array (e' # as))"
   | Var: "(Var _) \<leadsto> Error"
-(*  | Map: "\<lbrakk> (e1) \<leadsto> Res (Lam v e); (e2) \<leadsto> Res e2'; *)
+  | Map1: "\<lbrakk> (e1) \<leadsto> Res (Lam v e); (e2) \<leadsto> Res (Array []) \<rbrakk>
+          \<Longrightarrow> (Map e1 e2) \<leadsto> Res (Array [])"
+  | Map2: "\<lbrakk> (e1) \<leadsto> Res (Lam v e); (e2) \<leadsto> Res (Array (a # as));
+            (Map (Lam v e) (Array as)) \<leadsto> Res (Array as'); (Apply (Lam v e) a) \<leadsto> Res a' \<rbrakk>
+          \<Longrightarrow> (Map e1 e2) \<leadsto> Res (Array (a' # as'))"
+  | Zip1: "\<lbrakk> (e2) \<leadsto> Res (Array []) \<rbrakk>
+          \<Longrightarrow> (Zip e1 e2) \<leadsto> Res (Array [])"
+  | Zip2: "\<lbrakk> (e1) \<leadsto> Res (Array []) \<rbrakk>
+          \<Longrightarrow> (Zip e1 e2) \<leadsto> Res (Array [])"
+  | Zip3: "\<lbrakk> (e1) \<leadsto> Res (Array (a1 # a1s)); (e2) \<leadsto> Res (Array (a2 # a2s));
+            (Zip (Array a1s) (Array a2s)) \<leadsto> Res (Array as) \<rbrakk>
+          \<Longrightarrow> (Zip e1 e2) \<leadsto> Res (Array ((Tup a1 a2) # as))"
+(* TODO: Zip Fold Split Join *)
 
 end
